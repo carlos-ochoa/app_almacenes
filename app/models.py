1240e17a98f81b74dfab0e8584c5bfcb41a01198
carlos-ochoa <caog.freeman@gmail.com>
2021-01-30 20:01:26 -0600

    trabajo del dia 1

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from app import db
+
+class Operacion(db.Model):
+    __tablename__ = 'operaciones'
+
+    id = db.Column(db.Integer, primary_key = True)
+    concepto = db.Column(db.String(120))
+    fecha = db.Column(db.Date, index = True, default = datetime.today)
+    total = db.Column(db.Float)
+    billetes = db.Column(db.String(18))
+    tipo = db.Column(db.String(7), index = True)
+
+    def __repr__(self):
+        return f'Operacion de tipo {self.tipo} con total {self.total} y secuencia {self.billetes}'
+
+class Resumen(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    fecha = db.Column(db.Date, index = True, default = datetime.today)
+    cambio = db.Column(db.Float)
+    total = db.Column(db.Float)
+
+    def __repr__(self):
+        return f'Resumen de {self.fecha} con total {self.total} y cambio {self.cambio}'
